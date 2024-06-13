4df11c4659c201b7733b2cbf2839da414121e414
Robin Dervieux <robicode-05@users.noreply.github.com>
2024-06-13 23:24:38 +0200

    Add bdd to webapi project (test not passing)

--- webapi-tdd-exercice/webapi.test/Tests.fs
+++ webapi-tdd-exercice/webapi.test/Tests.fs
@@ -8,9 +8,13 @@ open FluentAssertions
 open System.Net
 open Program.webapi
 
+
+open Dapper.FSharp.SQLite
+open System.Collections
+
 let create () = (new WebApplicationFactory<Program>()).Server
 
-type Person = {
+type Product = {
     Name: string
     Price: int
 }
@@ -25,6 +29,19 @@ let ``Should return 200 ok when send product`` () =
         response.StatusCode.Should().Be(HttpStatusCode.OK, "Request should pass") |> ignore
     }
 
-// [<Fact>]
-// let ``Should add to storage when has new product`` () =
-//     let httpClient = create().CreateClient()
+[<Fact>]
+let ``Should add to storage when has new product`` () =
+    let httpClient = create().CreateClient()
+    task {
+        let! (response: Http.HttpResponseMessage) = httpClient.PostAsync(
+            "/products",
+            JsonContent.Create({ Name = "Souris"; Price = 55})) 
+
+        let! productsFromDb =
+            select {
+                for p in productTable do
+                selectAll
+            } |> connectionString.SelectAsync<Product>
+
+        productsFromDb.GetEnumerator().Current.Name.Should().Be("Souris", "Last input was Souris") |> ignore
+    }
--- webapi-tdd-exercice/webapi/Program.fs
+++ webapi-tdd-exercice/webapi/Program.fs
@@ -5,12 +5,36 @@ open System
 open Microsoft.AspNetCore.Http
 open Apis
 
+open Dapper.FSharp.SQLite
+open System.Data
+
+type Product = {
+    Name: string
+    Price: int
+}
+
+Dapper.FSharp.SQLite.OptionTypes.register()
+
+// let connectionString: IDbConnection = "Data Source=:memory:;Version=3;New=True;"
+let connectionString: IDbConnection =  new SQLite.SQLiteConnection("Data Source=:memory:;Version=3;New=True;")
+// let productTable = table<Product>
+let productTable = table'<Product> "products"
+
+let postProductInDB = 
+    Func<IResult>(fun () -> 
+        insert {
+            into productTable
+            value { Name = "François"; Price = 12 }
+        } |> connectionString.InsertAsync
+        |> ignore
+        Results.Ok()
+    )
 
 let builder = WebApplication.CreateBuilder()
 let app = builder.Build()
 
 app.MapGet("/", helloWorld) |> ignore
-app.MapPost("/products", postProduct) |> ignore
+app.MapPost("/products", postProductInDB) |> ignore
 
 app.Run()
 
